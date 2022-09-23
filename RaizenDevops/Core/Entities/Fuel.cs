d25defaada360c8c6b1f33e5d3bbd60938264ce9
Luis Henrique Gavirate <gavirate.henrique@gmail.com>
2022-09-22 22:39:15 -0300

    feat: add entities

---
+++ RaizenDevops/Core/Entities/Fuel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace RaizenDevops.Core.Entities
+{
+    public class Fuel
+    {
+        protected Fuel()
+        {
+
+        }
+
+        public Fuel(string description, int value, DateTime date) : base()
+        {
+            Description = description;
+            Value = value;
+            Date = date;
+        }
+
+        public string Description { get; private set; }
+        public int Value { get; private set; }
+        public DateTime Date { get; private set; }
+
+        public void Update(string description, int value, DateTime date)
+        {
+            Description = description;
+            Value = value;
+            Date = date;
+        }
+    }
+}
