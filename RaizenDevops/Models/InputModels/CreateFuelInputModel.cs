0a58e44e0b53c2e06693e5e5544b3d744adcd301
Luis Henrique Gavirate <gavirate.henrique@gmail.com>
2022-09-22 22:48:02 -0300

    feat: input models

---
+++ RaizenDevops/Models/InputModels/CreateFuelInputModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RaizenDevops.Models.InputModels
+{
+    public class CreateFuelInputModel
+    {
+        public string Description { get; set; }
+        public int Value { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
