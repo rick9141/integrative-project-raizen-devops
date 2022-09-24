e8ab1a36bc81efe63047dd43a0e035391516eb4d
Luis Henrique Gavirate <gavirate.henrique@gmail.com>
2022-09-24 04:53:11 -0300

    auto mapper and docs

--- RaizenDevops/Startup.cs
+++ RaizenDevops/Startup.cs
@@ -1,18 +1,16 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using RaizenDevops.API.Persistence.Repositories;
+using RaizenDevops.API.Profiles;
 using RaizenDevops.Persistence;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
 
 namespace RaizenDevops
 {
@@ -32,22 +30,31 @@ namespace RaizenDevops
 
             services.AddDbContext<RaizenDevopsDbContext>(o => o.UseSqlServer(connectionString));
 
+            services.AddScoped<IFuelRepository, FuelRepository>();
+
+            services.AddAutoMapper(typeof(FuelProfile));
+
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "RaizenDevops", Version = "v1" });
+
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
             });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "RaizenDevops v1"));
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "RaizenDevops v1"));
             }
 
             app.UseHttpsRedirection();
