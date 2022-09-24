b4ebda759f9ef022df2a6b98390b926bf7836b0a
Luis Henrique Gavirate <gavirate.henrique@gmail.com>
2022-09-24 02:22:26 -0300

    configure connections db

---
+++ RaizenDevops/Persistence/RaizenDevopsDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RaizenDevops.Core.Entities;
+
+namespace RaizenDevops.Persistence
+{
+    public class RaizenDevopsDbContext : DbContext
+    {
+        public RaizenDevopsDbContext(DbContextOptions<RaizenDevopsDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Fuel> Fuels { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Fuel>(f =>
+            {
+                f.HasKey(f => f.Id);
+            });
+        }
+    }
+}
--- RaizenDevops/Startup.cs
+++ RaizenDevops/Startup.cs
@@ -2,11 +2,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using RaizenDevops.Persistence;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,6 +28,10 @@ namespace RaizenDevops
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var connectionString = Configuration.GetValue<string>("RaizenDevopsConnectDb");
+
+            services.AddDbContext<RaizenDevopsDbContext>(o => o.UseSqlServer(connectionString));
+
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
