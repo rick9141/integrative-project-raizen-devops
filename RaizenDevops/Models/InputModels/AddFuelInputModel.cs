14dddc7275a34dbcf865edb7fe5bf142cd48f531
Luis Henrique Gavirate <gavirate.henrique@gmail.com>
2022-09-24 04:53:57 -0300

    adjust models

---
+++ RaizenDevops/Models/InputModels/AddFuelInputModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RaizenDevops.Models.InputModels
+{
+    public class AddFuelInputModel
+    {
+        public string FuelName { get; set; }
+        public string Description { get; set; }
+        public int Value { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
